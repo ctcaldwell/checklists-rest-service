7fd623bcb5fa7523e2f2bc2a4b4832fd8158f08c
Todd Caldwell <todd_caldwell@jabil.com>
2021-07-25 16:51:57 -0400

    Add ChecklistIntancesController

---
+++ src/main/java/com/example/checklists/controller/ChecklistInstancesController.java
@@ -0,0 +1,53 @@
+package com.example.checklists.controller;
+
+import com.example.checklists.domain.Checklist;
+import com.example.checklists.domain.ChecklistInstance;
+import com.example.checklists.repositories.ChecklistInstanceRepository;
+import com.example.checklists.repositories.ChecklistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Optional;
+
+@RestController
+public class ChecklistInstancesController {
+
+    @Autowired
+    private ChecklistRepository checklistRepository;
+
+    @Autowired
+    private ChecklistInstanceRepository checklistInstanceRepository;
+
+    @PostMapping("/checklists/{checklistId}/instances")
+    public ResponseEntity<ChecklistInstance> create(
+            UriComponentsBuilder uriBuilder,
+            @PathVariable(value = "checklistId") Long checklistId) {
+
+        ChecklistInstance newInstance = new ChecklistInstance();
+        ChecklistInstance persistedInstance = checklistRepository.findById(checklistId).map(checklist -> {
+            newInstance.setChecklist(checklist);
+            return checklistInstanceRepository.save(newInstance);
+        }).orElseThrow(() -> new ChecklistNotFoundException("ChecklistId " + checklistId + "not found"));
+
+        UriComponents uriComponents = uriBuilder.path("/checklists/{checklistId}/instances/{id}").buildAndExpand(checklistId, persistedInstance.getId());
+
+        return ResponseEntity.created(uriComponents.toUri()).body(persistedInstance);
+    }
+
+    @GetMapping("/checklists/{checklistId}/instances")
+    public Iterable<ChecklistInstance> getAllChecklistInstances(
+            @PathVariable(value = "checklistId") Long checklistId) {
+        return checklistInstanceRepository.findByChecklist(checklistRepository.findById(checklistId).map(checklist -> {
+            return checklist;
+        }).orElseThrow(() -> new ChecklistNotFoundException("ChecklistId " + checklistId + "not found")));
+    }
+
+    @GetMapping("/checklists/{checklistId}/instances/{id}")
+    public ResponseEntity<ChecklistInstance> getChecklistInstanceById(@PathVariable("id") Long id) {
+        Optional<ChecklistInstance> instanceOptional = checklistInstanceRepository.findById(id);
+        return ResponseEntity.of(instanceOptional);
+    }
+}
