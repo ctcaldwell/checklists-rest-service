fffef24066018b0cc7566f2e7ae21906d355bda9
Todd Caldwell <todd_caldwell@jabil.com>
2021-07-25 22:34:21 -0400

    Add GETs to controller tests

--- src/test/java/com/example/checklists/controller/TestRestControllers.java
+++ src/test/java/com/example/checklists/controller/TestRestControllers.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -24,13 +28,28 @@ public class TestRestControllers {
                 .contentType("application/json"))
                 .andExpect(status().isCreated());
 
+        mockMvc.perform(get("/checklists")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
         mockMvc.perform(post("/checklists/1/tasks")
                 .content("\"description\":\"Test Task 1\"")
                 .contentType("application/json"))
                 .andExpect(status().isCreated());
 
+        mockMvc.perform(get("/checklists/1/tasks")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
         mockMvc.perform(post("/checklists/1/instances")
                 .contentType("application/json"))
                 .andExpect(status().isCreated());
+
+        mockMvc.perform(get("/checklists/1/instances")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
